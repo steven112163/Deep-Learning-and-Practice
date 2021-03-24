183520f58d9bcbd002858def9f83aa7335a50e14
steven112163 <steven112163@gmail.com>
2021-03-24 14:30:06 +0800

    Add initial nn structure

--- Lab 1/backpropagation.py
+++ Lab 1/backpropagation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib as plt
 from typing import Tuple
+from argparse import ArgumentParser, Namespace
 
 
 def generate_linear(n: int = 100) -> Tuple[np.ndarray, np.ndarray]:
@@ -14,7 +15,6 @@ def generate_linear(n: int = 100) -> Tuple[np.ndarray, np.ndarray]:
 
     for pt in pts:
         inputs.append([pt[0], pt[1]])
-        distance = (pt[0] - pt[1]) / 1.414
         if pt[0] > pt[1]:
             labels.append(0)
         else:
@@ -23,7 +23,7 @@ def generate_linear(n: int = 100) -> Tuple[np.ndarray, np.ndarray]:
     return np.array(inputs), np.array(labels).reshape(n, 1)
 
 
-def generate_XOR_easy() -> Tuple[np.ndarray, np.ndarray]:
+def generate_xor_easy() -> Tuple[np.ndarray, np.ndarray]:
     """
     Generate data points based on XOR situation
     :return: inputs and labels
@@ -43,40 +43,86 @@ def generate_XOR_easy() -> Tuple[np.ndarray, np.ndarray]:
     return np.array(inputs), np.array(labels).reshape(21, 1)
 
 
-def sigmoid(x: np.ndarray) -> np.ndarray:
-    """
-    Calculate sigmoid function
-    :param x: input data
-    :return: sigmoid results
-    """
-    return 1.0 / (1.0 + np.exp(-x))
+class Layer:
+    def __init__(self):
+        self.weight = None
+        self.output = None
 
 
-def derivative_sigmoid(x: np.ndarray) -> np.ndarray:
-    """
-    Calculate the derivative of sigmoid function
-    :param x: input data
-    :return: derivative sigmoid result
-    """
-    return np.multiply(x, 1.0 - x)
+class NeuralNetwork:
+    def __init__(self, learning_rate: float = 0.7, hidden_units: int = 4, activation: str = 'sigmoid',
+                 convolution: bool = False):
+        self.learning_rate = learning_rate
+        self.hidden_units = hidden_units
+        self.activation = activation
+        self.convolution = convolution
+        self.layers = list()
+
+    @staticmethod
+    def sigmoid(x: np.ndarray) -> np.ndarray:
+        """
+        Calculate sigmoid function
+        :param x: input data
+        :return: sigmoid results
+        """
+        return 1.0 / (1.0 + np.exp(-x))
+
+    @staticmethod
+    def derivative_sigmoid(x: np.ndarray) -> np.ndarray:
+        """
+        Calculate the derivative of sigmoid function
+        :param x: input data
+        :return: derivative sigmoid result
+        """
+        return np.multiply(x, 1.0 - x)
+
+    @staticmethod
+    def show_result(x: np.ndarray, y: np.ndarray, pred_y: np.ndarray) -> None:
+        """
+        Show the ground truth and predicted results
+        :param x: data points
+        :param y: ground truth labels
+        :param pred_y: predicted labels
+        :return: None
+        """
+        plt.subplot(1, 2, 1)
+        plt.title('Ground truth', fontsize=18)
+        for idx, point in enumerate(x):
+            plt.plot(point[0], point[1], 'ro' if y[idx] == 0 else 'bo')
+
+        plt.subplot(1, 2, 2)
+        plt.title('Predict result', fontsize=18)
+        for idx, point in enumerate(x):
+            plt.plot(point[0], point[1], 'ro' if pred_y[idx] == 0 else 'bo')
+
+        plt.show()
 
 
-def show_result(x: np.ndarray, y: np.ndarray, pred_y: np.ndarray) -> None:
+def parse_arguments() -> Namespace:
     """
-    Show the ground truth and predicted results
-    :param x: data points
-    :param y: ground truth labels
-    :param pred_y: predicted labels
+    Parse arguments
+    :return: all arguments
+    """
+    parser = ArgumentParser()
+    parser.add_argument('--l', '--learning-rate')
+    parser.add_argument('--u', '--units', type=int)
+    parser.add_argument('--a', '--activation', type=int)
+    parser.add_argument('--c', '--convolution', default=False, type=bool)
+
+    return parser.parse_args()
+
+
+def main() -> None:
+    """
+    Main function
     :return: None
     """
-    plt.subplot(1, 2, 1)
-    plt.title('Ground truth', fontsize=18)
-    for idx, point in enumerate(x):
-        plt.plot(point[0], point[1], 'ro' if y[idx] == 0 else 'bo')
+    args = parse_arguments()
+    learning_rate = args.learning_rate
+    hidden_units = args.hidden_units
+    activation = args.activation
+    convolution = args.convolution
 
-    plt.subplot(1, 2, 2)
-    plt.title('Predict result', fontsize=18)
-    for idx, point in enumerate(x):
-        plt.plot(point[0], point[1], 'ro' if pred_y[idx] == 0 else 'bo')
 
-    plt.show()
+if __name__ == '__main__':
+    main()
