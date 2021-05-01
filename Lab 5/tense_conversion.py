d751fc413bd8f4ed4d4d54757f6b748032236780
steven112163 <steven112163@gmail.com>
2021-05-01 21:10:15 +0800

    Implement decoder

--- Lab 5/tense_conversion.py
+++ Lab 5/tense_conversion.py
@@ -3,7 +3,7 @@ from torch import optim, device, Tensor, LongTensor, cat, randn, exp
 from os import system
 from nltk.translate.bleu_score import SmoothingFunction, sentence_bleu
 from argparse import ArgumentParser, ArgumentTypeError, Namespace
-from typing import List
+from typing import List, Tuple
 import unicodedata
 import string
 import re
@@ -54,17 +54,16 @@ class EncoderRNN(nn.Module):
         self.cell_log_var = nn.Linear(in_features=hidden_size,
                                       out_features=latent_size)
 
-    def forward(self, inputs, prev_hidden, pre_cell, input_condition):
+    def forward(self, inputs: LongTensor, prev_hidden: Tensor, prev_cell: Tensor,
+                input_condition: int) -> Tuple[Tuple[Tensor, ...], Tuple[Tensor, ...]]:
         """
         Forward propagation
         :param inputs: inputs
         :param prev_hidden: previous hidden state
-        :param pre_cell: previous cell state
-        :param input_condition: input condition
-        :return: (hidden mean, hidden log variance, sampled hidden values), (cell mean, cell log variance, sampled cell values)
+        :param prev_cell: previous cell state
+        :param input_condition: input conditions
+        :return: (hidden mean, hidden log variance, hidden latent), (cell mean, cell log variance, cell latent)
         """
-        # TODO: add typing
-
         # Embed condition
         embedded_condition = self.embed_condition(input_condition)
 
@@ -72,7 +71,7 @@ class EncoderRNN(nn.Module):
         hidden_state = cat((prev_hidden, embedded_condition), dim=2)
 
         # Concatenate previous cell state with embedded condition to get current cell state
-        cell_state = cat((pre_cell, embedded_condition), dim=2)
+        cell_state = cat((prev_cell, embedded_condition), dim=2)
 
         # Embed inputs
         embedded_inputs = self.input_embedding(inputs).view(-1, 1, self.hidden_size)
@@ -83,14 +82,14 @@ class EncoderRNN(nn.Module):
         # Get hidden mean, log variance, and sampled values
         hidden_mean = self.hidden_mean(next_hidden)
         hidden_log_var = self.hidden_log_var(next_hidden)
-        hidden_values = randn(self.latent_size) * exp(0.5 * hidden_log_var) + hidden_mean
+        hidden_latent = randn(self.latent_size) * exp(0.5 * hidden_log_var) + hidden_mean
 
         # Get cell mean and log variance, and sampled values
         cell_mean = self.cell_mean(next_cell)
         cell_log_var = self.cell_log_var(next_cell)
-        cell_values = randn(self.latent_size) * exp(0.5 * cell_log_var) + cell_mean
+        cell_latent = randn(self.latent_size) * exp(0.5 * cell_log_var) + cell_mean
 
-        return (hidden_mean, hidden_log_var, hidden_values), (cell_mean, cell_log_var, cell_values)
+        return (hidden_mean, hidden_log_var, hidden_latent), (cell_mean, cell_log_var, cell_latent)
 
     def init_hidden_or_cell(self) -> Tensor:
         """
@@ -99,40 +98,95 @@ class EncoderRNN(nn.Module):
         """
         return torch.zeros(1, 1, self.hidden_size - self.condition_embedding_size, device=self.train_device)
 
-    def embed_condition(self, condition) -> Tensor:
+    def embed_condition(self, condition: int) -> Tensor:
         """
         Embed condition
         :param condition: original condition
         :return: embedded condition
         """
-        # TODO: add typing
         condition_tensor = LongTensor([condition]).to(self.train_device)
         return self.condition_embedding(condition_tensor).view(1, 1, -1)
 
 
 class DecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size, train_device):
+    def __init__(self, input_size: int, condition_size: int, hidden_size: int, latent_size: int,
+                 condition_embedding_size: int, train_device: device):
         super(DecoderRNN, self).__init__()
+
+        self.input_size = input_size
+        self.condition_size = condition_size
         self.hidden_size = hidden_size
+        self.latent_size = latent_size
+        self.condition_embedding_size = condition_embedding_size
         self.train_device = train_device
 
-        self.embedding = nn.Embedding(output_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
-        self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
+        # Embedding
+        self.condition_embedding = nn.Embedding(num_embeddings=condition_size,
+                                                embedding_dim=condition_embedding_size)
+        self.input_embedding = nn.Embedding(num_embeddings=input_size,
+                                            embedding_dim=hidden_size)
 
-    def forward(self, inputs, hidden):
-        output = self.embedding(inputs).view(1, 1, -1)
-        output = func.relu(output)
-        output, hidden = self.gru(output, hidden)
-        output = self.out(output[0])
-        return output, hidden
+        # Latent to hidden/cell
+        self.hidden_latent_to_hidden_state = nn.Linear(in_features=latent_size + condition_embedding_size,
+                                                       out_features=hidden_size)
+        self.cell_latent_to_cell_state = nn.Linear(in_features=latent_size + condition_embedding_size,
+                                                   out_features=hidden_size)
 
-    def init_hidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=self.train_device)
+        # RNN
+        self.lstm = nn.LSTM(input_size=hidden_size,
+                            hidden_size=hidden_size)
+
+        self.out = nn.Linear(hidden_size, input_size)
+
+    def forward(self, inputs: LongTensor, hidden_latent: Tensor, cell_latent: Tensor,
+                input_condition: int) -> Tuple[Tensor, ...]:
+        """
+        Forward propagation
+        :param inputs: inputs
+        :param hidden_latent: hidden latent
+        :param cell_latent: cell latent
+        :param input_condition: input conditions
+        :return: output, next hidden latent, next cell hidden latent
+        """
+        # Embed condition
+        embedded_condition = self.embed_condition(input_condition)
+
+        # Get hidden state and cell state
+        hidden_state, cell_state = self.init_hidden_and_cell(hidden_latent, cell_latent, embedded_condition)
+
+        # Embed inputs
+        embedded_inputs = self.input_embedding(inputs).view(1, 1, self.hidden_size)
+
+        # Get RNN outputs
+        output, next_hidden_latent, next_cell_latent = self.lstm(embedded_inputs, (hidden_state, cell_state))
+
+        # Get decoded output
+        output = self.out(output).view(-1, self.input_size)
+
+        return output, next_hidden_latent, next_cell_latent
+
+    def init_hidden_and_cell(self, hidden_latent: Tensor, cell_latent: Tensor,
+                             embedded_condition: Tensor) -> Tuple[Tensor, ...]:
+        """
+        Concatenate latent and condition, then convert latent to state and return
+        :param hidden_latent: hidden latent
+        :param cell_latent: cell latent
+        :param embedded_condition: embedded condition
+        :return: hidden state and cell state
+        """
+        concatenated_hidden_latent = torch.cat((hidden_latent, embedded_condition), dim=2)
+        concatenated_cell_latent = torch.cat((cell_latent, embedded_condition), dim=2)
+        return self.hidden_latent_to_hidden_state(concatenated_hidden_latent), self.cell_latent_to_cell_state(
+            concatenated_cell_latent)
 
 
 def gaussian_score(words: List[List[str]]) -> float:
+    """
+    Compute Gaussian score
+    :param words: words?
+    :return: score
+    """
+    # TODO: check words
     words_list = []
     score = 0
     with open('./data/train.txt', 'r') as fp:
@@ -148,6 +202,13 @@ def gaussian_score(words: List[List[str]]) -> float:
 
 
 def compute_bleu(output, reference) -> float:
+    """
+    Compute BLEU-4 score
+    :param output:
+    :param reference:
+    :return: BLEU-4 score
+    """
+    # TODO: add typing and check output and reference
     cc = SmoothingFunction()
     if len(reference) == 3:
         weights = (0.33, 0.33, 0.33)
