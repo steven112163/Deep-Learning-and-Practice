Fix kl loss

Lab 5/tense_conversion.py
@@ -381,8 +381,10 @@ def kl_loss(hidden_mean: Tensor, hidden_log_variance: Tensor, cell_mean: Tensor,
     :param cell_log_variance: log variance of cell state
     :return: loss
     """
-    return torch.sum(0.5 * (hidden_log_variance - cell_log_variance) + (
-            torch.exp(hidden_log_variance) + (hidden_mean - cell_mean) ** 2) / torch.exp(cell_log_variance) / 2.0 - 0.5)
+    # return torch.sum(0.5 * (hidden_log_variance - cell_log_variance) + (
+    #         torch.exp(hidden_log_variance) + (hidden_mean - cell_mean) ** 2) / torch.exp(cell_log_variance) / 2.0 - 0.5)
+    return torch.sum(0.5 * (hidden_mean ** 2 + torch.exp(hidden_log_variance) - hidden_log_variance - 1
+                            + cell_mean ** 2 + torch.exp(cell_log_variance) - cell_log_variance - 1))
 
 
 def show_results(epochs: int, losses_and_scores: Dict[str, List[float]]) -> None:
@@ -759,7 +761,8 @@ def train(input_size: int,
                 hidden_mean, hidden_log_var, hidden_latent = hidden
                 cell_mean, cell_log_var, cell_latent = cell
 
-                use_teacher_forcing = True if random.random() < losses_and_scores['Teacher forcing ratio'][epoch] else False
+                use_teacher_forcing = True if random.random() < losses_and_scores['Teacher forcing ratio'][
+                    epoch] else False
 
                 # Decode
                 decode_all_inputs = inputs[:-1]