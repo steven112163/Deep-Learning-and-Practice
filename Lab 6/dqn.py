612f2b8ad5a3861feee2089aec7736aab7390e26
steven112163 <steven112163@gmail.com>
2021-05-19 17:36:52 +0800

    Use 0.05 tau for soft update

--- Lab 6/dqn.py
+++ Lab 6/dqn.py
@@ -175,7 +175,7 @@ class DQN:
         # TODO
         self._target_net.load_state_dict(self._behavior_net.state_dict())
 
-    def _soft_update_target_network(self, tau=.005):
+    def _soft_update_target_network(self, tau=.05):
         """
         Update target network by _soft_ copying from behavior network
         :param tau: weight
