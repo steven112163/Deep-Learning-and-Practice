4ca0bb9ad5b31a6e0f5d172a8113fcb55c8ec77a
steven112163 <steven112163@gmail.com>
2021-05-19 23:12:32 +0800

    Use 0.9 tau for testing

--- Lab 6/dqn.py
+++ Lab 6/dqn.py
@@ -175,7 +175,7 @@ class DQN:
         # TODO
         self._target_net.load_state_dict(self._behavior_net.state_dict())
 
-    def _soft_update_target_network(self, tau=.25):
+    def _soft_update_target_network(self, tau=.9):
         """
         Update target network by _soft_ copying from behavior network
         :param tau: weight
