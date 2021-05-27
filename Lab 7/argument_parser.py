92bcdf786e0f1db3fcb418c69c0d608aa7655b0b
steven112163 <steven112163@gmail.com>
2021-05-27 18:07:57 +0800

    Update logging and default arguments

--- Lab 7/argument_parser.py
+++ Lab 7/argument_parser.py
@@ -19,13 +19,13 @@ def parse_arguments() -> Namespace:
     :return: arguments
     """
     parser = ArgumentParser(description='cGAN & cNF')
-    parser.add_argument('-b', '--batch_size', default=16, type=int, help='Batch size')
+    parser.add_argument('-b', '--batch_size', default=32, type=int, help='Batch size')
     parser.add_argument('-i', '--image_size', default=64, type=int, help='Image size')
-    parser.add_argument('-lrd', '--learning_rate_discriminator', default=0.001, type=float,
+    parser.add_argument('-lrd', '--learning_rate_discriminator', default=0.0001, type=float,
                         help='Learning rate of discriminator')
-    parser.add_argument('-lrg', '--learning_rate_generator', default=0.001, type=float,
+    parser.add_argument('-lrg', '--learning_rate_generator', default=0.0001, type=float,
                         help='Learning rate of generator')
-    parser.add_argument('-e', '--epochs', default=800, type=int, help='Number of epochs')
+    parser.add_argument('-e', '--epochs', default=200, type=int, help='Number of epochs')
     parser.add_argument('-v', '--verbosity', default=0, type=check_verbosity_type, help='Verbosity level')
 
     return parser.parse_args()
