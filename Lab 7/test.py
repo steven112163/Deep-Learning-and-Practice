e71cff2adbb7a6b4fd285e52063c9f7f4919cca0
steven112163 <steven112163@gmail.com>
2021-06-14 13:23:01 +0800

    Make latent and mean on the same device

--- Lab 7/test.py
+++ Lab 7/test.py
@@ -339,7 +339,6 @@ def generate_manipulated_images(train_dataset: CelebALoader,
             label = torch.from_numpy(label).to(training_device).type(torch.float).view(1, 40)
 
             z, _, _ = normalizing_flow.forward(x=image, x_label=label)
-            z = z.cpu().detach()
 
             if label[0, idx] == 1:
                 num_pos += 1
@@ -348,7 +347,6 @@ def generate_manipulated_images(train_dataset: CelebALoader,
                 num_neg += 1
                 neg_z_mean = (num_neg - 1) * neg_z_mean / num_neg + z / num_neg
     interval_z = 1.6 * (pos_z_mean - neg_z_mean)
-    interval_z = interval_z.to(training_device)
 
     alphas = [-1.0, -0.5, 0.0, 0.5, 1.0]
     for num_of_intervals, alpha in enumerate(alphas):
