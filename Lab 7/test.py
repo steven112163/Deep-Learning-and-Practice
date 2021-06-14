a3194a7770b4b926c0b8d78b0e65dd71a11c9645
steven112163 <steven112163@gmail.com>
2021-06-14 14:53:34 +0800

    Remove neg z mean from latent

--- Lab 7/test.py
+++ Lab 7/test.py
@@ -361,14 +361,13 @@ def generate_manipulated_images(data_loader: DataLoader,
         if len(neg_indices) > 0:
             num_neg += len(neg_indices)
             neg_z_mean = (num_neg - len(neg_indices)) / num_neg * neg_z_mean + z[neg_indices].sum(dim=0) / num_neg
-    interval_z = pos_z_mean - neg_z_mean
-    neg_z_mean = neg_z_mean.to(training_device)
+    interval_z = 1.6 * (pos_z_mean - neg_z_mean)
     interval_z = interval_z.to(training_device)
 
     alphas = [-1.0, -0.5, 0.0, 0.5, 1.0]
     for num_of_intervals, alpha in enumerate(alphas):
         with torch.no_grad():
-            image, _, _ = normalizing_flow.forward(x=latent + neg_z_mean + alpha * interval_z,
+            image, _, _ = normalizing_flow.forward(x=latent + alpha * interval_z,
                                                    x_label=neg_label + num_of_intervals * interval_label,
                                                    reverse=True)
         manipulated_images = torch.cat([manipulated_images,
